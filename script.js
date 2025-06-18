6898ebb2365556282a04af2ff1a8390d92f69dfc
temuriee <critstoggle@gmail.com>
2025-06-18 19:34:33 +0400

    s

---
+++ script.js
@@ -0,0 +1,28 @@
+let myObj = {
+  sayHello: function () {
+    console.log("Hello");
+  },
+};
+myObj.sayHello();
+
+let num = 0;
+
+while (num <= 10000) {
+  console.log(num);
+  num = num + 1;
+}
+let index = 0;
+let myFriendList = [
+  "ani",
+  "Girogi",
+  "Beqa",
+  "Mari",
+  "Saba",
+  "Merabi",
+  "Mindia",
+];
+
+while (index < myFriendList.length) {
+  console.log(myFriendList[index]);
+  index = index + 1;
+}
